0c5c4244bab2156a48ef77da0c1af4505f382c8c
shaddo24mc <shaddo24mc@gmail.com>
2026-04-08 16:29:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+const canvas = renderer.domElement;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 canvas.addEventListener('click', () => {
