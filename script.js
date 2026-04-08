afdaca66e87acdbf2172e72444e62222cb1c3f26
shaddo24mc <shaddo24mc@gmail.com>
2026-04-08 16:24:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+canvas.addEventListener('click', () => {
+  canvas.requestPointerLock();
+});
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),            // inline geometry
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }) // inline material
+  )
 scene.add(cube);
-
+const mouse{x: 0, y: 0 };
+document.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+});
 camera.position.z = 5;
-
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    camera.lookAt(mouse.x, mouse.y);
     renderer.render(scene, camera);
 }
 animate();
