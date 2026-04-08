abed5e21034ccfeeefbd95998e2ecc49293a9d88
shaddo24mc <shaddo24mc@gmail.com>
2026-04-08 16:34:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('mousemove', (event) => {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = - ((event.clientY / window.innerHeight) * 2 - 1);
 });
-
+camera.position.z = 5;
 // In animate function:
 function animate() {
   requestAnimationFrame(animate);
