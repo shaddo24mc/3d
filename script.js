c0be0ef4aa8d346750546dcdb16859cc9639d62e
shaddo24mc <shaddo24mc@gmail.com>
2026-04-09 15:43:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const targetlabel = document.GetElementById("targetl")
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
