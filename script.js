e2679754666679e67a4a0275acff42050d9201b6
lightcast <shaddo24mc@gmail.com>
2026-07-03 16:11:14 -0700

    asfd

--- script.js
+++ script.js
@@ -1008,9 +1008,9 @@ const HIDDEN_CREATIVE_BLOCKS = new Set([
     'pitcher_crop',
     'sweet_berry_bush',
 
-    'kelp_plant',
-    'weeping_vines_plant',
-    'twisting_vines_plant',
+    'kelp',
+    'weeping_vines',
+    'twisting_vines',
 
     'fire',
     'soul_fire',
