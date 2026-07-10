68a79489054219bdbcbd3b46d75b07d63865f004
lightcast <shaddo24mc@gmail.com>
2026-07-09 23:02:36 -0700

    heh heh heh

--- script.js
+++ script.js
@@ -2255,7 +2255,7 @@ async function loadCustomModel(bName, stateDict = {}, cacheKey = null) {
                             uvs.needsUpdate = true;
                         }
                         if (xRotDeg) geo.rotateX(-THREE.MathUtils.degToRad(xRotDeg));
-                        if (yRotDeg) geo.rotateY(-THREE.MathUtils.degToRad(yRotDeg + 90));
+                        if (yRotDeg) geo.rotateY(-THREE.MathUtils.degToRad(yRotDeg + 180));
                     }
 
                     allCompiledGeometries.push(geo);
