b56e8908ae7f85cc23754a27a068b775d45c9a8a
lightcast <shaddo24mc@gmail.com>
2026-07-11 15:08:01 -0700

    asdf

--- script.js
+++ script.js
@@ -2014,7 +2014,7 @@ async function loadCustomModel(bName, stateDict = {}, cacheKey = null) {
         }
         else if (bName.includes('bed_head')) {
             const parts = [
-                {size: [16, 16, 6], pos: [0, 3, 16], ...boxUV(0, 0, 16, 16, 6), pivot: [0, 3, 16], rot: [0, 0, 0]},
+                {size: [16, 16, 6], pos: [0, 3, 16], ...boxUV(0, 0, 16, 16, 6), pivot: [0, 3, 16], rot: [-90, 0, 0]},
                 {size: [3, 3, 3], pos: [0, 0, 0], ...boxUV(50, 0, 3, 3, 3)},
                 {size: [3, 3, 3], pos: [13, 0, 0], ...boxUV(50, 6, 3, 3, 3)}
             ]
@@ -2029,7 +2029,7 @@ async function loadCustomModel(bName, stateDict = {}, cacheKey = null) {
 
         else if (bName.includes('bed_foot')) {
             const parts = [
-                {size: [16, 16, 6], pos: [0, 3, 16], ...boxUV(6, 22, 16, 16, 6), pivot: [0, 3, 16], rot: [-90, 0, 0]},
+                {size: [16, 16, 6], pos: [0, 3, 16], ...boxUV(0, 22, 16, 16, 6), pivot: [0, 3, 16], rot: [-90, 0, 0]},
                 {size: [3, 3, 3], pos: [0, 0, 0], ...boxUV(53, 12, 3, 3, 3)},
                 {size: [3, 3, 3], pos: [13, 0, 0], ...boxUV(53, 18, 3, 3, 3)}
             ]
