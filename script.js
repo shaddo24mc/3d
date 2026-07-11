d1aadfbf7034960ea0ce6be6c5e9ce5935991973
lightcast <shaddo24mc@gmail.com>
2026-07-11 01:02:49 -0700

    har

--- script.js
+++ script.js
@@ -809,7 +809,7 @@ const baseBlocks = [
     'waxed_exposed_copper_chain', 
     'waxed_weathered_copper_chain', 
     'waxed_oxidized_copper_chain',
-    'chest_right', 'chest_left', 'trapped_right', 'trapped_left',
+    'chest_right', 'chest_left', 'trapped_right', 'trapped_left', 'red_bed_head',
 
     ...ITEMS
 ];
@@ -1857,7 +1857,7 @@ async function loadCustomModel(bName, stateDict = {}, cacheKey = null) {
         return;
     }
 
-    const hardcodedModels = new Set(['creeper_head', 'zombie_head', 'skeleton_skull', 'wither_skeleton_skull', 'dragon_head', 'player_head', 'chest', 'trapped_chest', 'ender_chest', 'chest_right', 'chest_left', 'trapped_right', 'trapped_left']);
+    const hardcodedModels = new Set(['creeper_head', 'zombie_head', 'skeleton_skull', 'wither_skeleton_skull', 'dragon_head', 'player_head', 'chest', 'trapped_chest', 'ender_chest', 'chest_right', 'chest_left', 'trapped_right', 'trapped_left', 'red_bed_head']);
     if (hardcodedModels.has(bName)) {
       try {
         const fallbackName = resolveFallbackTexture(bName);
