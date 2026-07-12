aadda7c0963698690fa673ad832c9f50d2a8b0cf
lightcast <shaddo24mc@gmail.com>
2026-07-12 16:38:46 -0700

    a

--- script.js
+++ script.js
@@ -2039,7 +2039,7 @@ async function loadCustomModel(bName, stateDict = {}, cacheKey = null) {
         else if (bName.includes('bed_foot')) {
             const parts = [
                 {size: [16, 16, 6], pos: [0, 3, -6], ...boxUV(0, 22, 16, 16, 6), pivot: [0, 3, 0], rot: [90, 0, 0], mirror: true, mirrorV: true, rotSouthNorth: true},
-                {size: [3, 3, 3], pos: [0, 0, 13], ...boxUV(50, 12, 3, 3, 3), pivot: [1.5, 1.5, 14.5], rot: [180, 0, 0], mirror: true, mirrorV: true, nsV: true, nsU: true, rotSouthNorth: true},
+                {size: [3, 3, 3], pos: [0, 0, 13], ...boxUV(50, 12, 3, 3, 3), pivot: [1.5, 1.5, 14.5], rot: [180, 0, 0], mirror: true, mirrorV: true, nsV: false, nsU: false, rotSouthNorth: true},
                 {size: [3, 3, 3], pos: [13, 0, 13], ...boxUV(50, 18, 3, 3, 3), pivot: [14.5, 1.5, 14.5], rot: [180, 90, 0], mirror: true, mirrorV: true, nsV: true, nsU: true, rotSouthNorth: true}
             ]
             let bedfootGeo = buildMCModel(parts, 64);
