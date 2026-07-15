51031281f6f14c92d2a033d5214a5f04e8dc3b81
lightcast <shaddo24mc@gmail.com>
2026-07-15 01:33:40 -0700

    a

--- script.js
+++ script.js
@@ -4265,7 +4265,7 @@ const MOB_MODELS = {
                 parent: null,
                 pivot: [0, 5, 2],
                 rot: [-90, 0, 0],
-                cubes: [ { texOffs: [18, 4], from: [-6, -10, -7], size: [12, 18, 10] } ]
+                cubes: [ { texOffs: [18, 4], from: [-6, -10, -7], size: [12, 18, 10], mirror: true } ]
             },
             head: {
                 parent: null,
