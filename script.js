f7d79dfcff84719d132853a2b00c6a97d9df10f9
lightcast <shaddo24mc@gmail.com>
2026-07-15 01:18:45 -0700

    dagUUna

--- script.js
+++ script.js
@@ -4264,6 +4264,7 @@ const MOB_MODELS = {
             body: {
                 parent: null,
                 pivot: [0, 5, 2],
+                rot; [-90, 0, 0]
                 cubes: [ { texOffs: [18, 4], from: [-6, -10, -7], size: [12, 18, 10] } ]
             },
             head: {
