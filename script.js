9786fa195cbb3d15b653954519f313b81f5e93fc
lightcast <shaddo24mc@gmail.com>
2026-07-16 20:49:31 -0700

    a

--- script.js
+++ script.js
@@ -4274,7 +4274,7 @@ const MOB_MODELS = {
                 pivot: [1, 10.5, -7],
                 cubes: [
                     {texOffs: [0, 0], from: [-3, 0, -2], size: [6, 6, 4]},
-                    {texOffs: [0, 10], from: [-1.5, -3, 7], size: [3, 3, 4], mirrorV: ['up']},
+                    {texOffs: [0, 10], from: [-1.5, 9, 7], size: [3, 3, 4], mirrorV: ['up']},
                     {texOffs: [16, 14], from: [-3, -2, -1], size: [2, 2, 1]},
                     {texOffs: [16, 14], from: [1, -2, -1], size: [2, 2, 1]}
                 ]
