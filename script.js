0700b1d9ad83d7d62a7de34248b3657e01522d2b
lightcast <shaddo24mc@gmail.com>
2026-07-18 16:30:52 -0700

    asdf

--- script.js
+++ script.js
@@ -4517,7 +4517,7 @@ const MOB_MODELS = {
         texH: 64,
         parts: {
             body: {parent: null, pivot: [0, 11, 9], rot: [-90, 0, 0], cubes: [{texOffs: [28, 8], from: [-5, -18, -1], size: [10, 16, 8]}, {texOffs: [28, 32], from: [-5, -18, -1], size: [10, 16, 8], inflate: 0.5}]},
-            head: {parent: null, pivot: [0, 12, -7], cubes: [{texOffs: [0, 0], from: [-4, -10, 0], size: [8, 8, 8]}, {texOffs: [16, 16], from: [-2, 3, 8], size: [4, 3, 1]}]},
+            head: {parent: null, pivot: [0, 12, -7], cubes: [{texOffs: [0, 0], from: [-4, -10, -8], size: [8, 8, 8]}, {texOffs: [16, 16], from: [-2, -6, -9], size: [4, 3, 1]}]},
             legFrontRight: {parent: null, pivot: [3, 6, -6], cubes: [{texOffs: [0, 16], from: [-2, 6, -2], size: [4, 6, 4]}]},
             legFrontLeft: {parent: null, pivot: [-3, 6, -6], cubes: [{texOffs: [0, 16], from: [-2, 6, -2], size: [4, 6, 4], mirrorU: ['north', 'west', 'east']}]},
             legBackRight: {parent: null, pivot: [3, 6, 6], cubes: [{texOffs: [0, 16], from: [-2, 6, -2], size: [4, 6, 4]}]},
