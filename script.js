c26fa6bff666bff55d4b302ef7a2142e3c21b4e4
lightcast <shaddo24mc@gmail.com>
2026-07-19 16:53:58 -0700

    asdf

--- script.js
+++ script.js
@@ -3702,7 +3702,7 @@ const BIOME_REGISTRY = [
 ];
 
 const chunkSize = 16;
-const renderDistance = 2; 
+const renderDistance = 10; 
 const worldHeight = 256;
 const minworldY = -64;
 
@@ -4550,7 +4550,7 @@ const MOB_MODELS = {
                 pivot: [0, 24, 0],
                 cubes: [
                     {texOffs: [0, 16], from: [-3, 25, -3], size: [6, 6, 6]},
-                    {texOffs: [32, 0], from: [-2.25, 26, -3], size: [2, 2, 2]},
+                    {texOffs: [32, 0], from: [-3.25, 26, -3], size: [2, 2, 2]},
                     {texOffs: [32, 4], from: [1.25, 26, -3], size: [2, 2, 2]},
                     {texOffs: [32, 8], from: [0, 29, -3], size: [1, 1, 1]},
                 ]
