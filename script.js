466303a5253400376d4355ee759340405fc86d16
lightcast <shaddo24mc@gmail.com>
2026-07-31 00:30:14 -0700

    asdf

--- script.js
+++ script.js
@@ -4336,9 +4336,9 @@ const MOB_MODELS = {
                 pivot: [1, 10.5, -7],
                 cubes: [
                     {texOffs: [0, 0], from: [-3, 0, -2], size: [6, 6, 4]},
-                    {texOffs: [0, 10], from: [-1.5, 2.99, -5], size: [3, 3, 4]},
-                    {texOffs: [16, 14], from: [-3, -2, 0], size: [2, 2, 1]},
-                    {texOffs: [16, 14], from: [1, -2, 0], size: [2, 2, 1]}
+                    {texOffs: [0, 10], from: [-1.5, -2.99, -5], size: [3, 3, 4]},
+                    {texOffs: [16, 14], from: [-3, 2, 0], size: [2, 2, 1]},
+                    {texOffs: [16, 14], from: [1, 2, 0], size: [2, 2, 1]}
                 ]
             },
             upperBody: {
@@ -4346,7 +4346,7 @@ const MOB_MODELS = {
                 pivot: [1, 10, 2],
                 rot: [-90, 0, 0],
                 cubes: [
-                    {texOffs: [21, 0], from: [-4, -8, -7], size: [8, 6, 7]}, //for -90 pivots: z neg: down, zpos: up, ypos: south, yneg: north
+                    {texOffs: [21, 0], from: [-4, 8, -7], size: [8, 6, 7]}, //for -90 pivots: z neg: down, zpos: up, ypos: south, yneg: north
                 ]
             },
             body: {
@@ -4354,13 +4354,13 @@ const MOB_MODELS = {
                 pivot: [0, 10, 2],
                 rot: [-90, 0, 0],
                 cubes: [
-                    {texOffs: [18, 14], from: [-2, -2, -7], size: [6, 9, 6]},
+                    {texOffs: [18, 14], from: [-2, 2, -7], size: [6, 9, 6]},
                 ]
             },
-            legFrontRight: {parent: null, pivot: [2.5, 8, -4], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
-            legFrontLeft: {parent: null, pivot: [-0.5, 8, -4], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
-            legBackRight: {parent: null, pivot: [2.5, 8, 7], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
-            legBackLeft: {parent: null, pivot: [-0.5, 8, 7], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
+            legFrontRight: {parent: null, pivot: [2.5, -8, -4], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
+            legFrontLeft: {parent: null, pivot: [-0.5, -8, -4], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
+            legBackRight: {parent: null, pivot: [2.5, -8, 7], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
+            legBackLeft: {parent: null, pivot: [-0.5, -8, 7], cubes: [{texOffs: [0, 18], from: [-1, 8, -1], size: [2, 8, 2]}]},
             tail: {parent: null, pivot: [1, 12, 8], rot: [-55, 0, 0], cubes: [{texOffs: [0, 18], from: [-1, 0, -1], size: [2, 8, 2]}]},
         }
     },
