e9f008c47086e2b9e27f1d4a6fc573de3261554e
lightcast <shaddo24mc@gmail.com>
2026-08-01 10:39:23 -0700

    a

--- script.js
+++ script.js
@@ -4498,7 +4498,7 @@ const MOB_MODELS = {
         texW: 64,
         texH: 64,//24.5
         parts: {
-            body: {parent: null, pivot: [0, 24, 0], cubes: [{texOffs: [0, 8], from: [-3, -1, -1], size: [5, 3, 9]}]},
+            body: {parent: null, pivot: [0, 24, 0], cubes: [{texOffs: [0, 8], from: [-3, -1, 15], size: [5, 3, 9]}]},
             head: {parent: 'body', pivot: [0, 23, -7], rot: [-12.5, 0, 0], emissiveOverlay: true, cubes: [{texOffs: [0, 0], from: [-4, -1, 0], size: [7, 3, 5]}]},
             tailBase: {parent: 'body', pivot: [0, 26, 1], rot: [5, 0, 0], cubes: [{texOffs: [3, 20], from: [-2, 2, 6], size: [3, 2, 6]}]},
             tailTip: {parent: 'tailBase', pivot: [0, 25.5, 7], rot: [5, 0, 0], cubes: [{texOffs: [4, 29], from: [-1, -1, -6], size: [1, 1, 6]}]},
