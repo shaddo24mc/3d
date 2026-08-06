1de1171853a04a381e5734e68a932b21dae5bb87
lightcast <shaddo24mc@gmail.com>
2026-08-06 14:16:33 -0700

    moo moo agoo

--- script.js
+++ script.js
@@ -4295,10 +4295,10 @@ const MOB_MODELS = {
                     { texOffs: [0, 32], from: [-3, -4, -7], size: [6, 3, 2] },
                 ]
             },
-            legFrontRight: { parent: null, pivot: [4, 12, -6], cubes: [ { texOffs: [0, 16], from: [-2, -12, -2], size: [4, 12, 4] } ] },
-            legFrontLeft:  { parent: null, pivot: [-4, 12, -6],  cubes: [ { uvUp: [4, 16], uvDown: [8, 16], uvEast: [0, 20], uvWest: [8, 20], uvNorth: [4, 20], uvSouth: [12, 20], from: [-2, -12, -2], size: [4, 12, 4], mirrorU: ['north', 'west', 'east'] } ] },
-            legBackRight:  { parent: null, pivot: [4, 12, 7],  cubes: [ { texOffs: [0, 16], from: [-2, -12, -2], size: [4, 12, 4] } ] },
-            legBackLeft:   { parent: null, pivot: [-4, 12, 7],   cubes: [ { uvUp: [4, 16], uvDown: [8, 16], uvEast: [0, 20], uvWest: [8, 20], uvNorth: [4, 20], uvSouth: [12, 20], from: [-2, -12, -2], size: [4, 12, 4], mirrorU: ['north', 'west', 'east'] } ] }
+            legFrontLeft: { parent: null, pivot: [-4, 12, -6], cubes: [ { texOffs: [0, 16], from: [-2, -12, -2], size: [4, 12, 4] } ] },
+            legFrontRight:  { parent: null, pivot: [4, 12, -6],  cubes: [ { uvUp: [4, 16], uvDown: [8, 16], uvEast: [0, 20], uvWest: [8, 20], uvNorth: [4, 20], uvSouth: [12, 20], from: [-2, -12, -2], size: [4, 12, 4], mirrorU: ['north', 'west', 'east'] } ] },
+            legBackLeft:  { parent: null, pivot: [-4, 12, 7],  cubes: [ { texOffs: [0, 16], from: [-2, -12, -2], size: [4, 12, 4] } ] },
+            legBackRight:   { parent: null, pivot: [4, 12, 7],   cubes: [ { uvUp: [4, 16], uvDown: [8, 16], uvEast: [0, 20], uvWest: [8, 20], uvNorth: [4, 20], uvSouth: [12, 20], from: [-2, -12, -2], size: [4, 12, 4], mirrorU: ['north', 'west', 'east'] } ] }
         }
     },
 
